9c78b1d40b3e82b6b14be9425bda3cbad992eef1
ddcctt <dylcota@hotmail.fr>
2015-06-12 12:19:28 +0200

    debut d'interface + changement mineur du code

--- projet.h
+++ projet.h
@@ -8,9 +8,17 @@
 #include<QTextStream>
 #include "timing.h"
 #include "tache.h"
-#include "exception.h"
 
 using namespace TIME;
+
+class ProjetException{
+public:
+    ProjetException(const QString& message):info(message){}
+    QString getInfo() const { return info; }
+private:
+    QString info;
+};
+
 
 //Statut = 0 => Projet non programmé, à venir
 class Projet {
@@ -29,12 +37,12 @@ class Projet {
         Projet(const Projet& t);
         Projet& operator=(const Projet& t);
         void addItem(Tache* t);
-
+    public:
         class IteratorSTL{
             private:
                 Tache** currentTache;
-                IteratorSTL(Tache** u): currentTache(u){};
             public:
+                IteratorSTL(Tache** u): currentTache(u){}
                 IteratorSTL operator++(){
                     ++currentTache;
                     return *this;
@@ -46,28 +54,22 @@ class Projet {
                 bool operator!=(const IteratorSTL& it) const {return currentTache!= it.currentTache;}
                 const Tache& operator*() const {return **currentTache;}
             };
-        class AProgIterator{
-        private:
-            Tache** currentTache;
-            unsigned int nb;
-        public:
-            AProgIterator(Tache** u):currentTache(u){};
-        };
 
-    public:
-        Projet(const QString& id, const QString& t, const QDate& disponible, const QDate& ech, int max=10) : statut(0),identificateur(id), titre(t), dispo(disponible), echeance(ech), nbMax(max), nb(0), taches(new Tache*[max]){};
+
+        Projet(const QString& id, const QString& t, const QDate& disponible, const QDate& ech, int max=10) : statut(0),identificateur(id), titre(t), dispo(disponible), echeance(ech), nbMax(max), nb(0), taches(new Tache*[max]){}
         ~Projet(){
             for(int i=0;i<nb;i++) delete taches[i];
             delete[] taches;}
-        QString getId() const {return identificateur;};
-        QString getTitre() const {return titre;};
-        QDate getDispo()const{return dispo;};
-        QDate getEcheance()const{return echeance;};
+        QString getId() const {return identificateur;}
+        QString getTitre() const {return titre;}
+        QDate getDispo()const{return dispo;}
+        QDate getEcheance()const{return echeance;}
         //A def
         Tache* getTache(const QString& id)const;
         void ajouterTache(const QString& desc, const QString& id, const QString& t,const Duree& du, const QDate& dispo, const QDate& deadline,bool preempt );
         IteratorSTL begin();
         IteratorSTL end();
+        void load(const QString& f);
         void save(const QString& f);
 };
 
