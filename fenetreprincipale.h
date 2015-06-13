375b15352246027374ff4e417248c0b4563e0393
ddcctt <dylcota@hotmail.fr>
2015-06-13 03:01:57 +0200

    Update fenetreprincipale.h

--- fenetreprincipale.h
+++ fenetreprincipale.h
@@ -6,11 +6,8 @@
 #include"tacheajout.h"
 #include"projetajout.h"
 #include"treeview.h"
-<<<<<<< HEAD
 #include"agenda.h"
-=======
 #include"activiteajout.h"
->>>>>>> 5c2cc245a30c417039aa7e583883766be910a42f
 class FenetrePrincipale : public QWidget
 {
     Q_OBJECT
@@ -22,11 +19,8 @@ public slots:
     void ouvrirAfficher();
     void ouvrirAjoutP();
     void ouvrirAjoutT();
-<<<<<<< HEAD
     void ouvrirAgenda();
-=======
     void ouvrirAjoutA();
->>>>>>> 5c2cc245a30c417039aa7e583883766be910a42f
 
 private:
     QPushButton* afficher;
